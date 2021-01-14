00f75da02cded532ee3e00dbec2a0dc7ec3756cf
nishantkj911 <38178615+nishantkj911@users.noreply.github.com>
2021-01-14 16:43:35 +0530

    typo

--- aws/main/modules/security/security_groups.tf
+++ aws/main/modules/security/security_groups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group_rule" "ssh_ingress" {
   from_port = 22
   to_port = 22
   protocol = "tcp"
-  security_group_id = aws_security_group.instance_access
+  security_group_id = aws_security_group.instance_access.id
 }
 
 output "sg_id" {
