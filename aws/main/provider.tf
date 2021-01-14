2dc6e7aafb63f393dceb35136e7e822333c340d4
nishantkj911 <38178615+nishantkj911@users.noreply.github.com>
2021-01-14 13:22:34 +0530

    Added access management

--- aws/main/provider.tf
+++ aws/main/provider.tf
@@ -10,4 +10,6 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1" // Mumbai region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
--- aws/main/variables.tf
+++ aws/main/variables.tf
@@ -1,3 +1,11 @@
 variable "instance_type" {
   default = "t2.micro"
 }
+
+variable "access_key" {
+  description = "The AWS access key of the gen account"
+}
+
+variable "secret_key" {
+  description = "The secret of the provided access key"
+}
